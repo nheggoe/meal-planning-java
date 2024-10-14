b4a62bef6eeae7243899297e160d5251ce380ae0
Nick Heggø <git@nheggoe.dev>
2024-10-14 14:17:08 +0200

    test git

---
+++ src/main/java/edu/ntnu/idi/bidata/Main.java
@@ -0,0 +1,11 @@
+package edu.ntnu.idi.bidata;
+
+/**
+ * The main starting point of your application. Let this class create the
+ * instance of your main-class that starts your application.
+ */
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hellow world");
+  }
+}
